4b29e426711d39c7418d74d98886018333c3d563
Youssef Bouhaddi <youssef_bouhaddi@hotmail.fr>
2024-04-16 12:44:24 +0100

    First commit

---
+++ ActivPrat_2/Readme.md
@@ -0,0 +1,5 @@
+# TP2: Activite pratique n°1 - ORM JPA HIBERNATE SPRING DATA.
+
+## screen de la bd mysql 
+
+![img]()
---
+++ ActivPrat_2/src/main/java/com/fahd/ActivitePratique2/entities/Product.java
@@ -0,0 +1,21 @@
+package com.fahd.ActivitePratique2.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private double price;
+    private int quantity;
+
+}
